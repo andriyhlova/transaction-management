bf4ab8a1eafbc006730794dd50278ffe24b45a73
Andrii Hlova <ahlova@paysign.com>
2026-04-21 22:30:48 +0300

    new changes

---
+++ TransactionManagement.UnitTests/Tests/TransactionManagementServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using TransactionManagement.BLL.Services.Implementations;
+using TransactionManagement.DAL.Entities;
+using TransactionManagement.DAL.Repositories.Interfaces;
+
+namespace TransactionManagement.UnitTests.Tests
+{
+    public class TransactionManagementServiceTests
+    {
+        //[Fact]
+        //public async Task GetTotalForDateAsync_WhenDateIsValid_ShouldReturnValidAmout()
+        //{
+        //    //Arrange
+
+        //    Mock<ITransactionRepository> transactionRepository = new Mock<ITransactionRepository>();
+        //    transactionRepository.Setup(m => m.GetAllAsync()).ReturnsAsync(new List<TransactionEntity>
+        //    {
+        //        new TransactionEntity{ Id = 1, Date= new DateTime(2023, 04, 11), Amount = 2 },
+        //            new TransactionEntity{ Id = 2, Date= new DateTime(2023, 04, 11), Amount = 8 },
+        //            new TransactionEntity{ Id = 3, Date= new DateTime(2023, 04, 10), Amount = 2 }
+        //    });
+
+        //    var transactionService = new TransactionService(transactionRepository.Object);
+
+
+        //    //Act
+
+        //    var actualTotal = await transactionService.GetTotalForDateAsync(new DateTime(2023, 04, 11));
+
+        //    //Assert
+
+        //    Assert.Equal(10, actualTotal);
+        //}
+
+        [Theory]
+        [InlineData(11, 10)]
+        [InlineData(10, 2)]
+        [InlineData(9, 0)]
+        public async Task GetTotalForDateAsync_WhenDateIsValid_ShouldReturnValidAmout(int day, decimal expectedTotal)
+        {
+            //Arrange
+
+            Mock<ITransactionRepository> transactionRepository = new Mock<ITransactionRepository>();
+            transactionRepository.Setup(m => m.GetAllAsync()).ReturnsAsync(new List<TransactionEntity>
+            {
+                new TransactionEntity{ Id = 1, Date= new DateTime(2023, 04, 11), Amount = 2 },
+                    new TransactionEntity{ Id = 2, Date= new DateTime(2023, 04, 11), Amount = 8 },
+                    new TransactionEntity{ Id = 3, Date= new DateTime(2023, 04, 10), Amount = 2 }
+            });
+
+            var transactionService = new TransactionService(transactionRepository.Object);
+
+
+            //Act
+
+            var actualTotal = await transactionService.GetTotalForDateAsync(new DateTime(2023, 04, day));
+
+            //Assert
+
+            Assert.Equal(expectedTotal, actualTotal);
+        }
+    }
+}
--- sonar.bat
+++ sonar.bat
@@ -1,7 +1,7 @@
-dotnet sonarscanner begin /k:"TransactionManagement" /d:sonar.login=sqp_3daa99eebb8bc82aa28ad0af6ff1ae343779e737 /d:sonar.host.url=http://localhost:9000 /d:sonar.verbose=true  /d:sonar.cs.vstest.reportsPaths="TransactionManagement.UnitTests1/TestResults/results.trx" /d:sonar.cs.opencover.reportsPaths=coverage.xml
+dotnet sonarscanner begin /k:"andriyhlova_transaction-management" /o:andriyhlova /d:sonar.login=789ed7c677047140608469a22763ec4c1db2251e /d:sonar.host.url=https://sonarcloud.io /d:sonar.verbose=true  /d:sonar.cs.vstest.reportsPaths="TransactionManagement.UnitTests/TestResults/results.trx" /d:sonar.cs.opencover.reportsPaths=coverage.xml
 
 dotnet build TransactionManagement.sln
 
-dotnet test ./TransactionManagement.UnitTests1 --no-build --logger:"trx;logfilename=results.trx" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=../coverage.xml
+dotnet test ./TransactionManagement.UnitTests --no-build --logger:"trx;logfilename=results.trx" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=../coverage.xml
 
-dotnet sonarscanner end /d:sonar.login=sqp_3daa99eebb8bc82aa28ad0af6ff1ae343779e737
+dotnet sonarscanner end /d:sonar.login=789ed7c677047140608469a22763ec4c1db2251e
