45e5e9e5d479a24c881f8dde91a67c51fd1b0c16
Andrii Hlova <a.hlova@gmail.com>
2026-04-22 19:31:29 +0300

    added new changes for CI/CD

--- TransactionManagement.Web/Program.cs
+++ TransactionManagement.Web/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
 using TransactionManagement.BLL.Services.Implementations;
 using TransactionManagement.BLL.Services.Interfaces;
@@ -9,9 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 if (builder.Environment.IsProduction())
 {
-    //builder.Configuration.AddAzureKeyVault(
-    //    new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
-    //    new DefaultAzureCredential(new DefaultAzureCredentialOptions()));
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential(new DefaultAzureCredentialOptions()));
 }
 
 //test
